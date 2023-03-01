741262f60341ff9948803d67ad8142b82060148d
Simon Salazar Albornoz <simon.salazar.a@gmail.com>
2023-03-01 11:50:16 -0300

    team update

--- projects/astromer/components/header.js
+++ projects/astromer/components/header.js
@@ -12,7 +12,7 @@ class Header extends HTMLElement {
     			<ul>
     				<li><a href="${this.getAttribute('root')}index.html" accesskey="1" title="">Get started</a></li>
     				<li><a href="${this.getAttribute('root')}pages/about_astromer.html" accesskey="2" title="">About ASTROMER</a></li>
-    				<li><a href="https://astromer.readthedocs.io/en/new_doc/" accesskey="3" title="">Documentation</a></li>
+    				<li><a href="https://astromer.readthedocs.io/en/main/" accesskey="3" title="">Documentation</a></li>
 					<li><a href="${this.getAttribute('root')}pages/team.html" accesskey="4" title="">Team</a></li>
     				<li><a href="${this.getAttribute('root')}pages/contact.html" accesskey="5" title="">Contact Us</a></li>
 					
